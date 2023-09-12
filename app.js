e7aae04389aab170d6cc3ae1bd3a065f06c77a6a
sheriff-o <sheriffafolabi95@gmail.com>
2023-09-12 15:07:55 +0100

    edit status code

--- app.js
+++ app.js
@@ -33,7 +33,7 @@ app.get("/get_intern_info", (req, res) => {
     track: track,
     github_file_url: fileGithubUrl,
     github_repo_url: fullSourceCodeUrl,
-    status_code: "success",
+    status_code: 200,
   };
 
   res.json(response);
